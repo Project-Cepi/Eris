28852ef01ed7cac2366fa1c299a75535580e68f7
CmdrNorthpaw <thetuxkid@gmail.com>
2020-10-21 13:54:36 +0100

    Initialize Discord API on start

--- src/main/kotlin/world/cepi/chatextension/ChatExtension.kt
+++ src/main/kotlin/world/cepi/chatextension/ChatExtension.kt
@@ -2,12 +2,15 @@ package world.cepi.chatextension
 
 import com.beust.klaxon.Klaxon
 import net.minestom.server.extensions.Extension;
+import org.javacord.api.DiscordApi
+import org.javacord.api.DiscordApiBuilder
 import java.io.File
 
 class ChatExtension : Extension() {
 
     override fun initialize() {
         logger.info("[ExampleExtension] has been enabled!")
+        logger.info("[ChatExtension] Your discord bot can be invited with this link: ${discord?.createBotInvite()}")
     }
 
     override fun terminate() {
@@ -24,4 +27,6 @@ class ChatExtension : Extension() {
             DiscordConfig()
         } else klaxon.parse<DiscordConfig>(configFile.readText())!!
     }
+
+    val discord: DiscordApi? = if (config.enabled) DiscordApiBuilder().setToken(config.token).login().join() else null
 }
