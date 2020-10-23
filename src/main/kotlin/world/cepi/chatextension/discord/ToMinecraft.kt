e2503cfcaee364fecf963002351d753a71027709
CmdrNorthpaw <thetuxkid@gmail.com>
2020-10-23 09:30:44 +0100

    RichMessage used correctly now

--- src/main/kotlin/world/cepi/chatextension/discord/ToMinecraft.kt
+++ src/main/kotlin/world/cepi/chatextension/discord/ToMinecraft.kt
@@ -11,8 +11,8 @@ fun discordToChat(event: MessageCreateEvent) {
     if (event.messageAuthor.isYourself) return
 
     for (player in MinecraftServer.getConnectionManager().onlinePlayers) {
-        MinecraftServer.getConnectionManager().broadcastMessage(RichMessage()
-                .append(ColoredText.of("${ChatColor.BOLD}[${ChatColor.PURPLE}DISCORD${ChatColor.WHITE}] ${ChatColor.NO_COLOR}<${event.messageAuthor.displayName}> ${event.messageContent}"))
+        MinecraftServer.getConnectionManager().broadcastMessage(RichMessage
+                .of(ColoredText.of("${ChatColor.BOLD}[${ChatColor.PURPLE}DISCORD${ChatColor.WHITE}] ${ChatColor.NO_COLOR}<${event.messageAuthor.displayName}> ${event.messageContent}"))
                 .setClickEvent(ChatClickEvent.openUrl(event.messageLink.ref))
                 .setHoverEvent(ChatHoverEvent.showText(ColoredText.of(ChatColor.PURPLE, "Message from the Cepi discord! Come join us at [discord link]")))
         )
