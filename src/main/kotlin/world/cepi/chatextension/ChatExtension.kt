ba594efb6d4a2e00085e6c14da90babe66c5d5ce
CmdrNorthpaw <thetuxkid@gmail.com>
2020-10-24 19:27:54 +0100

    Idiot me didn't register events soooooo

--- src/main/kotlin/world/cepi/chatextension/ChatExtension.kt
+++ src/main/kotlin/world/cepi/chatextension/ChatExtension.kt
@@ -3,16 +3,25 @@ package world.cepi.chatextension
 import com.beust.klaxon.Klaxon
 import com.google.gson.Gson
 import com.google.gson.JsonParser
+import net.minestom.server.MinecraftServer
+import net.minestom.server.entity.Player
+import net.minestom.server.event.player.PlayerChatEvent
 import net.minestom.server.extensions.Extension;
+import net.minestom.server.listener.ChatMessageListener
 import org.javacord.api.DiscordApi
 import org.javacord.api.DiscordApiBuilder
+import world.cepi.chatextension.discord.chatToDiscord
 import java.io.File
 
 class ChatExtension : Extension() {
 
+    private val connectionManager = MinecraftServer.getConnectionManager()
+
     override fun initialize() {
         logger.info("[ExampleExtension] has been enabled!")
         logger.info("[ChatExtension] Your discord bot can be invited with this link: ${discord?.createBotInvite()}")
+
+        connectionManager.addPlayerInitialization {player -> registerEvents(player) }
     }
 
     override fun terminate() {
@@ -34,4 +43,8 @@ class ChatExtension : Extension() {
         val discord: DiscordApi? = if (config.enabled) DiscordApiBuilder().setToken(config.token).login().join() else null
 
     }
+}
+
+fun registerEvents(player: Player) {
+    player.addEventCallback(PlayerChatEvent::class.java) {event -> chatToDiscord(event)}
 }
