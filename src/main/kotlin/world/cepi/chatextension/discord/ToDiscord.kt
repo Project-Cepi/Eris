ec409d2c2729f3616b5f23295af6f13af27cf55f
CmdrNorthpaw <thetuxkid@gmail.com>
2020-10-26 09:24:30 +0000

    Fiddled around a bit with Javacord

--- src/main/kotlin/world/cepi/chatextension/discord/ToDiscord.kt
+++ src/main/kotlin/world/cepi/chatextension/discord/ToDiscord.kt
@@ -3,16 +3,22 @@ package world.cepi.chatextension.discord
 import net.minestom.server.MinecraftServer
 import net.minestom.server.event.player.PlayerChatEvent
 import org.javacord.api.entity.channel.ChannelType
+import org.javacord.api.entity.message.Message
+import org.javacord.api.util.logging.ExceptionLogger
 import world.cepi.chatextension.ChatExtension
 
 fun chatToDiscord(event: PlayerChatEvent) {
     val config = ChatExtension.config
     val discord = ChatExtension.discord ?: return
-    val channelOptional = discord.getChannelById(config.channel.toLong())
-    if (channelOptional.isPresent || channelOptional.get().type != ChannelType.SERVER_TEXT_CHANNEL) {
-        MinecraftServer.getLOGGER().info("ERROR! Channel ${config.channel} is not a valid channel!")
+    val channelOptional = discord.getChannelById(config.channel)
+    if (!channelOptional.isPresent) {
+        MinecraftServer.getLOGGER().info("ERROR! Channel ${config.channel} does not exist")
+        return
+    } else if (channelOptional.get().type != ChannelType.SERVER_TEXT_CHANNEL) {
+        MinecraftServer.getLOGGER().info("Channel type invalid")
         return
     }
     val channel = channelOptional.get().asTextChannel().get()
     channel.sendMessage("**<${event.sender.displayName}>** ${event.message}")
+            .exceptionally(ExceptionLogger.get())
 }
