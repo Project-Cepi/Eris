e7be0eceac8a843d313ba943555cf83b8fdc70e0
CmdrNorthpaw <thetuxkid@gmail.com>
2020-10-26 15:03:52 +0000

    Null problem fix

--- src/main/kotlin/world/cepi/chatextension/discord/ToMinecraft.kt
+++ src/main/kotlin/world/cepi/chatextension/discord/ToMinecraft.kt
@@ -1,5 +1,6 @@
 package world.cepi.chatextension.discord
 
+import io.netty.handler.codec.DecoderException
 import net.minestom.server.MinecraftServer
 import net.minestom.server.chat.*
 import org.javacord.api.event.message.MessageCreateEvent
@@ -11,14 +12,10 @@ class DiscordToChat : MessageCreateListener {
         val config = ChatExtension.config
         if (event.channel.id != config.channel) return
         if (event.messageAuthor.isYourself) return
-
-        for (player in MinecraftServer.getConnectionManager().onlinePlayers) {
-            MinecraftServer.getConnectionManager().broadcastMessage(RichMessage
-                    .of(ColoredText.of("${ChatColor.BOLD}[${ChatColor.PURPLE}DISCORD${ChatColor.WHITE}] ${ChatColor.NO_COLOR}<${event.messageAuthor.displayName}> ${event.messageContent}"))
-                    .setClickEvent(ChatClickEvent.openUrl(event.messageLink.ref))
-                    .setHoverEvent(ChatHoverEvent.showText(ColoredText.of(ChatColor.PURPLE, "Message from the Cepi discord! Come join us at [discord link]")))
-            )
-        }
+        MinecraftServer.getConnectionManager().broadcastMessage(RichMessage
+                .of(ColoredText.of("${ChatColor.BOLD}[${ChatColor.PURPLE}DISCORD${ChatColor.WHITE}] ${ChatColor.NO_COLOR}<${event.messageAuthor.displayName}> ${event.messageContent}"))
+                .setClickEvent(ChatClickEvent.openUrl(event.messageLink.toString()))
+                .setHoverEvent(ChatHoverEvent.showText(ColoredText.of(ChatColor.PURPLE, "Message from the Cepi discord! Come join us at [discord link]"))))
     }
 }
 
