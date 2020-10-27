10d8e5ab9228607a72f6f365304faddfcb8aef5d
CmdrNorthpaw <thetuxkid@gmail.com>
2020-10-27 20:30:30 +0000

    Object-based event listened

--- src/main/kotlin/world/cepi/chatextension/discord/ToMinecraft.kt
+++ src/main/kotlin/world/cepi/chatextension/discord/ToMinecraft.kt
@@ -3,16 +3,19 @@ package world.cepi.chatextension.discord
 import net.minestom.server.MinecraftServer
 import net.minestom.server.chat.*
 import org.javacord.api.event.message.MessageCreateEvent
+import org.javacord.api.event.server.ServerJoinEvent
+import org.javacord.api.listener.message.MessageCreateListener
 import world.cepi.chatextension.ChatExtension
 
-fun discordToChat(event: MessageCreateEvent) {
-    val config = ChatExtension.config
-    if (event.channel.id != config.channel) return
-    if (event.messageAuthor.isYourself) return
-    MinecraftServer.getConnectionManager().broadcastMessage(RichMessage
-            .of(ColoredText.of("${ChatExtension.discordPrefix} <${event.messageAuthor.displayName}> ${event.messageContent}"))
-            .setClickEvent(ChatClickEvent.openUrl(event.messageLink.ref))
-            .setHoverEvent(ChatHoverEvent.showText(ColoredText.of(ChatColor.PURPLE, "Message from the Cepi discord! Come join us at [discord link]")))
+class DiscordToChat : MessageCreateListener {
+    override fun onMessageCreate(event: MessageCreateEvent) {
+        val config = ChatExtension.config
+        if (event.channel.id != config.channel) return
+        if (event.messageAuthor.isYourself) return
+        MinecraftServer.getConnectionManager().broadcastMessage(RichMessage
+                .of(ColoredText.of("${ChatExtension.discordPrefix} <${event.messageAuthor.displayName}> ${event.messageContent}"))
+                .setClickEvent(ChatClickEvent.openUrl(event.messageLink.ref))
+                .setHoverEvent(ChatHoverEvent.showText(ColoredText.of(ChatColor.PURPLE, "Message from the Cepi discord! Come join us at [discord link]")))
         )
-
+    }
 }
