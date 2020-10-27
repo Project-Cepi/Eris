24f9806d7083da1aad40fcc5363bb61422410044
CmdrNorthpaw <thetuxkid@gmail.com>
2020-10-27 18:04:25 +0000

    Join leave logging!

--- src/main/kotlin/world/cepi/chatextension/discord/ToDiscord.kt
+++ src/main/kotlin/world/cepi/chatextension/discord/ToDiscord.kt
@@ -1,9 +1,14 @@
 package world.cepi.chatextension.discord
 
 import net.minestom.server.MinecraftServer
+import net.minestom.server.entity.Player
 import net.minestom.server.event.player.PlayerChatEvent
 import org.javacord.api.entity.channel.ChannelType
+import org.javacord.api.entity.message.embed.Embed
+import org.javacord.api.entity.message.embed.EmbedBuilder
 import world.cepi.chatextension.ChatExtension
+import java.awt.Color
+import java.io.File
 
 fun chatToDiscord(event: PlayerChatEvent) {
     val config = ChatExtension.config
@@ -15,4 +20,41 @@ fun chatToDiscord(event: PlayerChatEvent) {
     }
     val channel = channelOptional.get().asTextChannel().get()
     channel.sendMessage("**<${event.sender.displayName ?: event.sender.username}>** ${event.message}")
+}
+
+private fun joinLeaveEmbed(player: Player, type: EmbedType): EmbedBuilder = EmbedBuilder()
+            .setColor(if (type == EmbedType.JOIN) Color.green else Color.RED)
+            .setTitle("${player.displayName ?: player.username} has joined the game!")
+            .setColor(Color.GREEN)
+            .setThumbnail("https://minotar.net/avatar/helm/${player.uuid.toString().replace("-", "")}/128.png")
+
+fun onJoin(player: Player) {
+    if (ChatExtension.discord == null) return
+    val embed = joinLeaveEmbed(player, EmbedType.JOIN)
+    val channelOptional = ChatExtension.discord.getChannelById(ChatExtension.config.channel)
+    if (!channelOptional.isPresent || channelOptional.get().type != ChannelType.SERVER_TEXT_CHANNEL) {
+        MinecraftServer.getLOGGER().info("Error! Configured channel ${ChatExtension.config.channel} is invalid!")
+        return
+    }
+    val channel = channelOptional.get()
+
+    channel.asServerTextChannel().get().sendMessage(embed)
+}
+
+fun onLeave(player: Player) {
+    if (ChatExtension.discord == null) return
+    val embed = joinLeaveEmbed(player, EmbedType.LEAVE)
+    val channelOptional = ChatExtension.discord.getChannelById(ChatExtension.config.channel)
+    if (!channelOptional.isPresent || channelOptional.get().type != ChannelType.SERVER_TEXT_CHANNEL) {
+        MinecraftServer.getLOGGER().info("Error! Configured channel ${ChatExtension.config.channel} is invalid!")
+        return
+    }
+    val channel = channelOptional.get()
+
+    channel.asServerTextChannel().get().sendMessage(embed)
+}
+
+private enum class EmbedType {
+    LEAVE,
+    JOIN
 }
