5cc5644764e01f7aced525d0bbb9296bfcaf1cd1
LeoDog896 <LeoDog896@hotmail.com>
2020-11-03 19:54:51 -0500

    bump version

--- build.gradle.kts
+++ build.gradle.kts
@@ -37,7 +37,7 @@ dependencies {
     testImplementation("org.junit.jupiter:junit-jupiter:5.6.0")
 
     // Compile Minestom into project
-    implementation("com.github.Minestom:Minestom:master-SNAPSHOT")
+    implementation("com.github.Minestom:Minestom:b84bcde84d")
 
     // Use the Netty library
     implementation("io.netty:netty-transport-native-epoll:4.1.52.Final")
