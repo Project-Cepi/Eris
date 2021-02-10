1539ecaa135fd705c6012c06aa36dde9dd14334e
LeoDog896 <LeoDog896@hotmail.com>
2021-02-10 15:39:50 -0500

    Allow player pinging

--- src/main/kotlin/world/cepi/chatextension/events/FormattedChat.kt
+++ src/main/kotlin/world/cepi/chatextension/events/FormattedChat.kt
@@ -1,9 +1,14 @@
 package world.cepi.chatextension.events
 
+import net.minestom.server.MinecraftServer
+import net.minestom.server.chat.ChatColor
 import net.minestom.server.chat.ChatColor.*
 import net.minestom.server.event.player.PlayerChatEvent
+import net.minestom.server.sound.Sound
+import net.minestom.server.sound.SoundCategory
 import world.cepi.chatextension.emojis.Emoji
 import world.cepi.kstom.asRich
+import world.cepi.kstom.plus
 
 fun styleFormattedChat(event: PlayerChatEvent) {
     event.setChatFormat { chatEvent ->
@@ -11,6 +16,13 @@ fun styleFormattedChat(event: PlayerChatEvent) {
         var message = chatEvent.message
         Emoji.emojis.forEach { message = message.replace(":${it.name}:", it.value) }
 
-        "$DARK_GRAY[$GRAY${chatEvent.player.level}$DARK_GRAY] $WHITE${chatEvent.player.displayName ?: chatEvent.player.username} $DARK_GRAY>> $WHITE$message".asRich()
+        MinecraftServer.getConnectionManager().onlinePlayers.forEach {
+            if (message.contains(it.username)) {
+                message = message.replace(it.username, YELLOW + it.username + WHITE)
+                it.playSound(Sound.BLOCK_NOTE_BLOCK_PLING, SoundCategory.PLAYERS, 1f, 2f)
+            }
+        }
+
+        return@setChatFormat "$DARK_GRAY[$GRAY${chatEvent.player.level}$DARK_GRAY] $WHITE${chatEvent.player.displayName ?: chatEvent.player.username} $DARK_GRAY>> $WHITE$message".asRich()
     }
 }
