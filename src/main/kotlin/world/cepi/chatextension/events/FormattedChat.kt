09c37d677acf0e502f998262b95098e58f1f0564
LeoDog896 <LeoDog896@hotmail.com>
2021-05-26 09:43:08 -0400

    Format chat formatting

--- src/main/kotlin/world/cepi/chatextension/events/FormattedChat.kt
+++ src/main/kotlin/world/cepi/chatextension/events/FormattedChat.kt
@@ -13,7 +13,9 @@ import net.minestom.server.event.player.PlayerChatEvent
 import net.minestom.server.sound.SoundEvent
 import world.cepi.chatextension.emojis.Emoji
 import world.cepi.kstom.Manager
-
+import world.cepi.kstom.util.component1
+import world.cepi.kstom.util.component2
+import world.cepi.kstom.util.component3
 
 val miniMessageFormat = MiniMessage.builder()
     .removeDefaultTransformations()
@@ -22,51 +24,51 @@ val miniMessageFormat = MiniMessage.builder()
     .markdownFlavor(DiscordFlavor.get())
     .build()
 
-fun styleFormattedChat(event: PlayerChatEvent) {
-    event.setChatFormat { chatEvent ->
+// TODO w h a t. (please format this code or something i dont know god)
+fun styleFormattedChat(event: PlayerChatEvent) = event.setChatFormat { chatEvent ->
+    val messageComponent = miniMessageFormat.parse(chatEvent.message)
+        .let {
+            // Fold the players, replacing the initial value's text one by one
+            return@let Manager.connection.onlinePlayers.fold(it) { acc, player ->
+                return@fold acc.replaceText(
+                    TextReplacementConfig.builder()
+                        .matchLiteral(player.username)
+                        .replacement { _ ->
 
-        val messageComponent = miniMessageFormat.parse(chatEvent.message)
-            .let {
-                // Fold the players, replacing the initial value's text one by one
-                return@let Manager.connection.onlinePlayers.fold(it) { acc, player ->
-                    return@fold acc.replaceText(
-                        TextReplacementConfig.builder()
-                            .matchLiteral(player.username)
-                            .replacement { _ ->
+                            val (x, y, z) = player.position
 
-                                player.playSound(Sound.sound(
-                                    SoundEvent.BLOCK_NOTE_BLOCK_PLING,
-                                    Sound.Source.PLAYER,
-                                    1f, 2f)
-                                )
+                            player.playSound(Sound.sound(
+                                SoundEvent.BLOCK_NOTE_BLOCK_PLING,
+                                Sound.Source.PLAYER,
+                                1f, 2f,
+                            ), x, y, z)
 
-                                Component.text(player.username, NamedTextColor.YELLOW)
-                                    .append(Component.empty().color(NamedTextColor.WHITE))
+                            Component.text(player.username, NamedTextColor.YELLOW)
+                                .append(Component.empty().color(NamedTextColor.WHITE))
 
-                            }.build())
-                }
+                        }.build())
             }
-            .let {
-                // Fold the emojis, replacing the initial value's text one by one
-                return@let Emoji.emojis.fold(it) { acc, emoji ->
-                    return@fold acc.replaceText(
-                        TextReplacementConfig
-                            .builder()
-                            .matchLiteral(":${emoji.name}:").replacement(
-                                Component.text(emoji.value)
-                            ).build()
-                    )
-                }
+        }
+        .let {
+            // Fold the emojis, replacing the initial value's text one by one
+            return@let Emoji.emojis.fold(it) { acc, emoji ->
+                return@fold acc.replaceText(
+                    TextReplacementConfig
+                        .builder()
+                        .matchLiteral(":${emoji.name}:").replacement(
+                            Component.text(emoji.value)
+                        ).build()
+                )
             }
+        }
 
-        return@setChatFormat Component.text("[", NamedTextColor.DARK_GRAY)
-            .append(Component.text(chatEvent.player.level, NamedTextColor.GRAY))
-            .append(Component.text("]", NamedTextColor.DARK_GRAY))
-            .append(Component.space())
-            .append(Component.text(chatEvent.player.username, NamedTextColor.WHITE))
-            .append(Component.space())
-            .append(Component.text(">>", NamedTextColor.GRAY))
-            .append(Component.space())
-            .append(messageComponent.color(NamedTextColor.WHITE))
-    }
+    return@setChatFormat Component.text("[", NamedTextColor.DARK_GRAY)
+        .append(Component.text(chatEvent.player.level, NamedTextColor.GRAY))
+        .append(Component.text("]", NamedTextColor.DARK_GRAY))
+        .append(Component.space())
+        .append(Component.text(chatEvent.player.username, NamedTextColor.WHITE))
+        .append(Component.space())
+        .append(Component.text(">>", NamedTextColor.GRAY))
+        .append(Component.space())
+        .append(messageComponent.color(NamedTextColor.WHITE))
 }
